79a79a6025acb2cda2925eb49d38c52ee198e593
we1vi <fangvivi@qq.com>
2021-05-19 18:22:28 +0800

    完成字符串部分的练习17

--- src/main/java/com/wayne/strings/StringsExercise17.java
+++ src/main/java/com/wayne/strings/StringsExercise17.java
@@ -17,22 +17,42 @@ import static com.wayne.utils.Print.print;
  * @author wayne
  */
 public class StringsExercise17 {
+    /**
+     * (?x) Comments 注释中的空格和正则表达式中#到行尾的内容会被忽略
+     * (?m) Multiline 匹配 行尾结束符或者输入字符串的中 ^ 之后的部分；行尾结束符或者输入字符串的中 $ 之前的部分
+     * (?s) Dotall 匹配所有的字符，包括行尾结束符
+     * */
+    static final  String reg = "(?x)(?m)(?s)/\\*(.*?)\\*/|//(.*?)$";
     public static void main(String[] args) {
-        String reg = "[?m//|/\\*.*\\*/]";
         File file = new File(args[0]);
         Pattern p = Pattern.compile(reg);
-        Matcher m = p.matcher("");
-        int index = 0;
-        for (String str : new TextFile(args[0])){
-            m.reset(str);
-            while (m.find()){
-                print(index++ + " : " + m.group() + " : " + m.start());
-            }
-
+        String read = TextFile.read(args[0]);
+        Matcher m = p.matcher(read);
+        while (m.find()){
+            System.out.println("groupCount : " + m.groupCount());
+            System.out.println(m.group(1) != null ? m.group(1) : m.group(0));
         }
-
     }
-
-
 }
+
+/*
+会输出正则表达式的一部分内容
+输出：
+groupCount : 2
+        *
+        ********************** Exercise 17 *********************
+        * Write a program that reads a Java source-code file (you
+        * provide the file name on the command line) and displays
+        * all the comments.
+        ********************************************************
+        * @author wayne
+
+        groupCount : 2
+        *
+        * (?x) Comments 注释中的空格和正则表达式中#到行尾的内容会被忽略
+        * (?m) Multiline 匹配 行尾结束符或者输入字符串的中 ^ 之后的部分；行尾结束符或者输入字符串的中 $ 之前的部分
+        * (?s) Dotall 匹配所有的字符，包括行尾结束符
+        *
+        groupCount : 2
+//(.*?)$";*/
 
