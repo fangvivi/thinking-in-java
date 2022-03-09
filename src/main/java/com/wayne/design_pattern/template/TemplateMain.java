519fc913e578c1d0c2adf256b2efd6d60748c27b
kernel <fine-way@outlook.com>
2022-03-09 15:46:28 +0800

    模板模式，删除多余代码

--- src/main/java/com/wayne/design_pattern/template/TemplateMain.java
+++ src/main/java/com/wayne/design_pattern/template/TemplateMain.java
@@ -1,10 +1,5 @@
 package com.wayne.design_pattern.template;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.util.Arrays;
-
 /**
  * 模板模式
  * @author wayne
@@ -17,17 +12,5 @@ public class TemplateMain {
         game = new Football();
         game.play();
 
-        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-        try (ObjectOutputStream output = new ObjectOutputStream(buffer)) {
-            // 写入int:
-            output.writeInt(12345);
-            // 写入String:
-            output.writeUTF("Hello");
-            // 写入Object:
-            output.writeObject(new Football());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println(Arrays.toString(buffer.toByteArray()));
     }
 }
